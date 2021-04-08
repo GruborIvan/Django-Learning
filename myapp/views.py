211734a707e6ffa9bef015c7ff397b4b46732182
Ivan <ivan.grubor@gmail.com>
2021-04-08 16:15:50 +0200

    #CSCM-31-Migrations and Models task done

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from myapp.models import Person
 from django.http import HttpResponse
+import json
 # Create your views here.
 
+# Hardcode radi testiranja views..
+bookList = Person('Ivan','Grubor',22,'343621897');
+
 def index(request):
-    return HttpResponse("Hello world from Ivan's first Django application!");
+    return HttpResponse('This is the main page of my application!');
+
+# request is out request object.
+def about(request):
+    return HttpResponse('Hello, this is the about page!');
